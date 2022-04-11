6f2a4e0f49beae93143adc7e05bbdfaf8958aeda
inoveAlumnos <alumnos@inove.com.ar>
2022-04-11 18:24:37 -0300

    Upload files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# ahorcado_python
-Código de ejemplo de implementación del juego del ahorcado
+![Inove banner](/inove.jpg)
+Inove Escuela de Código\
+info@inove.com.ar\
+Web: [Inove](http://inove.com.ar)
+
+# Generala! [Python]
+En este repositorio se encuentra la implementación del juego del ahorcado, ejemplo integrador del curso de python inicial.
+
+En el archivo __interfaz.py__ se encuentra la función "dibujar" para dibujar en consola el ahorcado.
+
+En el archivo __ahorcado.py__ deberá llevar adelante la implementación del mismo.
---
+++ ahorcado.py
@@ -0,0 +1,36 @@
+# Python Inicial [Python]
+# Ejercicio integrador
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+import interfaz
+
+if __name__ == "__main__":
+    print("¡Aquí comienza el juego del ahorcado!")
+
+    # Leer la palabra secreta de un archivo csv...
+
+    # Armar un bucle que funcione hasta que el jugador
+    # gane o pierda
+
+    # Dentro del bucles ...
+    
+    # 1)
+    # Llamar a la funcion "dibujar" dentro del módulo "interfaz"
+    # con los parámetros necesarios para dibujar el juego
+    # Esto se realiza al principio del bucle para que la primera vez
+    # el usuario pueda ver la cantidad de letras de la palabra secreta
+    # La primera vez la mayoria de las variables que se brinden
+    # estarán vacias y se irán completando y modificando en las próximas funciones
+
+    # 2)
+    # Pedir al usuario una letra, almacenarla en la lista de letras usadas
+    # Si la letra ya estaba en la lista, pedir una nueva
+
+    # 3) 
+    # Verificar si la letra es parte de la palabra secreta
+    # En función de eso contabilizar la cantidad de intentos invalidos
+
+    print("¡Terminó el juego!")
+
