68924997c51b33c6037b92aaa61e1003f5e07390
Emmanuel Torres Molina <eotorresmolina@gmail.com>
2022-04-16 15:32:17 -0300

    Agregada funciones

--- solucion_ahorcado.py
+++ solucion_ahorcado.py
@@ -45,6 +45,49 @@ def get_letra():
     return letra
 
 
+def get_palabra_oculta(palabra_oculta):
+    '''
+    Obtiene de la lista "palabra_oculta"
+    las letras, retornando un string de esa
+    palabra.
+    '''
+    palabra = ''
+    for i in range(len(palabra_oculta)):
+        # Como la letra viene con el caracter seguido
+        # de un '.', me quedo sólo con la letra.
+        palabra += palabra_oculta[i][0:-1]
+    return palabra
+
+
+def ahorcado(palabra):
+    '''
+    Función que comprueba si el jugador ganó o no la partida
+    dependiendo si adivinó la "palabra secreta" o si perdió
+    porque superó el máximo de intentos permitidos.
+    '''
+    # Inicializo las variables y listas a utilizar.
+    intentos = 0
+    letras_usadas = []
+    es_ganador = False     
+    
+    # Esto se realiza para que el jugador pueda ver al principio
+    # la cantidad de letras de la palabra a adivinar.
+    interfaz.dibujar(palabra, letras_usadas, intentos)
+    return es_ganador
+
+
+def imprimir_mensaje_resultado(es_ganador, nro_partida):
+    '''
+    Muestra un mensaje en consola dependiendo
+    si el jugador ganó o no la partida.
+    '''
+    if es_ganador:
+        print(f'\n¡Usted ah ganado la partida número {nro_partida+1}!\n')
+    else:
+        print('\n¡Ahorcado!')
+        print(f'¡Usted ah perdido la partida número {nro_partida+1}!\n')
+
+
 if __name__ == "__main__":
     print("\n¡Aquí comienza el juego del ahorcado!\n")
 
@@ -57,6 +100,8 @@ if __name__ == "__main__":
         print('Máximo intentos permitidos:', MAX_CANT_INTENTOS)
         
         palabra_adivinar = palabras_secretas[i]['palabras']
+        es_ganador = ahorcado(palabra_adivinar)
+        imprimir_mensaje_resultado(es_ganador, i)
     
     print('\nNo quedan más palabras.')
     print("¡Terminó el juego!\n\n")
