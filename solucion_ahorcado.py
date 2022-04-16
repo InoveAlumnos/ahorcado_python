3e42c7e731285354bc2d4d69c726a36cabadc5e5
Emmanuel Torres Molina <eotorresmolina@gmail.com>
2022-04-16 14:36:17 -0300

    Upload file

---
+++ solucion_ahorcado.py
@@ -0,0 +1,20 @@
+# Python Inicial [Python]
+# Ejercicio integrador
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# Importamos módulos y librerías necesarias.
+import interfaz
+import csv
+
+
+
+if __name__ == "__main__":
+    print("\n¡Aquí comienza el juego del ahorcado!\n")
+
+    # Leer la/las palabras secretas5 de un archivo csv.
+    
+    print('\nNo quedan más palabras.')
+    print("¡Terminó el juego!\n\n")
+
